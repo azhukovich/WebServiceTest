ac3f991c57855f2e1a17ac4a38e404e2306dc1db
U_M1R7R <AVZhukovich@alfabank.ru>
2026-07-13 13:04:01 +0300

    first commit

---
+++ src/main/java/com/example/demo/DataController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class DataController {
+
+    @PostMapping("/data")
+    public String receiveData(@RequestBody String body) {
+        System.out.println("Received: " + body);
+        return "OK: " + body;
+    }
+}
