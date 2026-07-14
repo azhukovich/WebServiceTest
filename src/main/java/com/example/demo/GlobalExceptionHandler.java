c171b5d7efeb200207019673d415affc819b00d3
Alex Z <alex.zhukovich@gmail.com>
2026-07-14 13:10:21 +0300

    error handling added

---
+++ src/main/java/com/example/demo/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        model.addAttribute("stackTrace", ex.getStackTrace());
+        return "error";
+    }
+}
