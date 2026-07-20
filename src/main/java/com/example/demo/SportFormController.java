3ad6e9d0d8f9058f85593b231801a206605d147b
Alex Z <alex.zhukovich@gmail.com>
2026-07-21 00:24:03 +0300

    local db + week table added

--- src/main/java/com/example/demo/SportFormController.java
+++ src/main/java/com/example/demo/SportFormController.java
@@ -4,10 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 @Controller
 public class SportFormController {
+
+    private final SportRepository sportRepo;
+
+    public SportFormController(SportRepository sportRepo) {
+        this.sportRepo = sportRepo;
+    }
 
     @GetMapping("/sportform")
     public String sportForm(Model model) {
@@ -16,6 +26,56 @@ public class SportFormController {
         List<String> names = List.of("Отжимания", "Приседания");
 
         model.addAttribute("names", names);
+
+        //Результаты за неделю
+        LocalDate weekAgo = LocalDate.now().minusDays(7);
+        LocalDate today = LocalDate.now().plusDays(1);
+
+        List<LocalDate> days = weekAgo
+                .datesUntil(today.plusDays(1))
+                .collect(Collectors.toList());
+
+        Map<LocalDate, List<Integer>> grouped = sportRepo.findAll().stream()
+                .filter(r -> {
+                    LocalDate d = r.getCreatedAt().toLocalDate();
+                    return !d.isBefore(weekAgo) && !d.isAfter(today);
+                })
+                .collect(Collectors.groupingBy(
+                        r -> r.getCreatedAt().toLocalDate(),
+                        Collectors.mapping(
+                                r-> r.getQuantity(),
+                                Collectors.toList()
+                        )
+                ));
+
+        Map<LocalDate, String> resultStrs = grouped.entrySet().stream()
+                        .collect(Collectors.toMap(
+                                Map.Entry::getKey,
+        e->{
+            List<Integer> values = e.getValue();
+
+            String left = values.stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining("+"));
+
+            int sum = values.stream()
+                    .mapToInt(Integer::intValue)
+                    .sum();
+
+            return left + "=" + sum;
+        }));
+
+        TreeMap<LocalDate, String> finalMap = days.stream()
+                .collect(Collectors.toMap(
+                        d -> d,
+                        d -> resultStrs.getOrDefault(d, "Нет результатов"),
+                        (oldValue, newValue) -> oldValue, // Слияние при совпадении ключей
+                        TreeMap::new
+                ))
+                ;
+
+        model.addAttribute("resultsByDay", finalMap);
+
         return "sportform";
     }
 }
